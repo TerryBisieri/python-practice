af58f97579e6e663ace1bd2aca0839cd2690db7b
TerryBisieri <terryanne.kinanga@strathmore.edu>
2025-04-25 10:54:31 +0300

    Check if a number is even or odd

---
+++ even_or_odd.py
@@ -0,0 +1,8 @@
+# function to check if a number is even or odd
+def check_even_or_odd(number):
+  if number % 2 == 0:
+    return "Even"
+  else:
+    return "Odd"
+    
+print(check_even_or_odd(8))
