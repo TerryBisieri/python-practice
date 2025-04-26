92bd3a7c77084c54f11b37ca1230e0630faeec42
TerryBisieri <terryanne.kinanga@strathmore.edu>
2025-04-26 20:40:12 +0300

    Create reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,8 @@
+# Function to reverse a string manually
+def reverse_string(s):
+  reversed_str = ''
+  for char in s:
+    reversed_str = char + reversed_str
+  return reversed_str
+
+print(reverse_string("Please"))
